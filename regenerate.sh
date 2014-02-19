c70b55b5abd8d299cf882f775248113db6a594a9
Shoghi Cervantes <shoghicp@gmail.com>
2014-02-19 03:46:02 +0100

    Added multiple libraries

--- regenerate.sh
+++ regenerate.sh
@@ -3,6 +3,8 @@ set -e
 dpkg-deb -b net.pocketmine.server
 dpkg-deb -b net.pocketmine.php-5.5
 dpkg-deb -b net.pocketmine.php-5.5-opcache
+dpkg-deb -b net.pocketmine.zlib
+dpkg-deb -b net.pocketmine.libyaml
 
 dpkg-scanpackages . /dev/null > Packages
 cat Packages | bzip2 -9c > Packages.bz2
